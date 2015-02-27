f4b5365d0bd5b5505e88d21d936238e4a24ba936
Roger Camps <rogercampsny@gmail.com>
2015-02-26 16:45:46 -0800

    improve books index design

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
+    @books = Book.where(availability: true) # we are displaying only available books
   end
 
   # GET /books/1
